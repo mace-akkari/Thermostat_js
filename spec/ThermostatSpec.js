bf66596c4dbee0c97c241f42330b37360c2711ab
Mace <maceplus@gmail.com>
2020-11-06 15:20:01 +0000

    add minimum temperature function

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -22,6 +22,12 @@ describe('Thermostat',() => {
     expect(thermostat.getCurrentTemperature()).toEqual(19);
   })
 
+  it('has a minimum of 10 degrees', () => {
+    for (let i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
 
 
 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,9 +2,10 @@
 
 class Thermostat {
   constructor() {
+    this.MINIMUM_TEMPERATURE = 10;
     this.temperature = 20;
   }
-
+  
   getCurrentTemperature() {
     return this.temperature;
   }
@@ -14,10 +15,14 @@ class Thermostat {
   }
 
   down() {
-    this.temperature -= 1 
+    if (this.isMinimumTemperature()) {
+      return;
+    }
+    this.temperature -= 1
   }
 
-
-
+  isMinimumTemperature() {
+    return this.temperature === this.MINIMUM_TEMPERATURE;
+  }
 
 }
