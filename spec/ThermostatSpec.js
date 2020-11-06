c9846b8f06d1d2f4d8026dd48ca51db73815c3be
Mace <maceplus@gmail.com>
2020-11-06 12:35:55 +0000

    Thermostat starts at 20 degrees

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('Thermostat',() => {
+
+  let thermostat;
+
+  beforeEach(() => {
+    thermostat = new Thermostat();
+  });
+
+  it('starts at 20 degrees', () => {
+    expect(thermostat.temperature).toEqual(20);
+  });
+});
---
+++ src/Thermostat.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class   Thermostat {
+  constructor() {
+    this.temperature = 20;
+
+  }
+
+
+
+
+}
