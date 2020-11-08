d5d8a9c56891e29826c07e0a39f3c8bd160be508
Mace <maceplus@gmail.com>
2020-11-08 00:18:51 +0000

    add API

--- src/interface.js
+++ src/interface.js
@@ -2,6 +2,18 @@ $(document).ready( () => {
   var thermostat = new Thermostat();
   updateTemperature();
 
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  $('#current-temperature').text(data.main.temp);
+  })
+
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  $('#temperature-high').text(data.main.temp_max);
+  })
+
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  $('#temperature-low').text(data.main.temp_min);
+  })
+
   $('#temperature-up').click( () =>  {
     thermostat.up();
     updateTemperature();
